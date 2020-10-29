39411f7cdcb859370493fc1d4b40a54d25d4be8f
MAXIMILIAN REHBERGER <maximilian.rehberger@belectric.com>
2020-10-29 17:01:59 +0100

    ubuntu.sh updated

--- ubuntu-install-test/ubuntu.sh
+++ ubuntu-install-test/ubuntu.sh
@@ -3,5 +3,5 @@
 mkdir logs
 
 docker build -t ubuntu-test-install:latest . 2>&1 | tee logs/build.log
-docker run ubuntu-test-install 2>&1 | tee -a logs/install.log
+docker run ubuntu-test-install 2>&1 | tee logs/install.log
 
